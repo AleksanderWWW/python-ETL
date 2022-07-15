5e6568cb537233b98992b12b70897889736c5e5f
Aleksander Wojnarowicz <a.wojnarowicz@oerlikon.com>
2022-07-15 14:48:58 +0200

    add reading config

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 """
 Main runnable module
 """
+import yaml
+
+# read config
+with open("config.yaml", "r", encoding="utf-8") as config_fp:
+    _CONFIG = yaml.safe_load(config_fp)
+
+
+def main():
+    ...
