9f56251fb591125aa15d0cc790a4819072fae3ff
AleksanderWWW <alwojnarowicz@gmail.com>
2022-07-21 10:32:10 +0200

    implement pipeline by modyfing class attributes rather than injecting them

--- etl/src.py
+++ etl/src.py
@@ -2,33 +2,55 @@
 ETL for data coming from Bank of Canada API
 """
 
-import os
+import sys
 import json
+import sqlite3
+import decimal
 
 from pathlib import Path
 from datetime import date, datetime
-from typing import (
-    Union, 
-    Dict, 
-    Any, 
-    Tuple, 
-    List
-)
+from abc import ABC, abstractmethod, abstractproperty
+from typing import Union, Any, Tuple 
 
 import petl
 import dropbox
 import requests
 
-class Extract:
-    """
-    Class providing methods to connect to Bank of Canada API and extract the FX USD/CAD data.
-    This class encapsulates the EXTRACT part of the ETL process.
-    """
+
+class PipelineComponent(ABC):
+
+    @abstractproperty
+    def result(self) -> Union[Tuple[Any], None]:
+        """
+        This property exposes the result of any step of the process pipeline.
+        It will be passed on to the next step as input.
+        """
+        ...
+
+    @abstractmethod
+    def run(self) -> None:
+        """
+        This method will be run after instantiating each process component in the pipeline.
+        It implements the entire logic of any given step.
+        E.g. Extract subclass will implement data extraction logic in this method.
+        It will therefore make use of other methods in the class that implement
+        all the steps to extract the data, like make request to API or download
+        file from Dropbox.
+        """
+        ...
+
+
+class Extract(PipelineComponent):
     _DATE_FMT = "%Y-%m-%d"
     _DATETIME_FMT = "%m-%d-%Y-%H:%M:%S"
+    _PATH_TO_EXPENSES = "data/expenses.xlsx"
 
-    def __init__(self, api_url: str, dbx: dropbox.Dropbox,
-                 start_date: date, end_date: Union[date, None] = None):
+    def __init__(self, 
+                 api_url: str, 
+                 dbx: dropbox.Dropbox,
+                 start_date: date, 
+                 end_date: Union[date, None] = None) -> None:
+                 
         self.api_url_template = api_url
         self.dbx = dbx
 
@@ -44,6 +66,11 @@ class Extract:
 
         self.api_url: str = self._create_api_url()
 
+        self.exchange_rates_dict: dict
+        self.api_call_status_code: int
+
+        self.expenses_petl_table: petl.Table
+
     def _create_api_url(self) -> str:
         try:
             api_url = self.api_url_template.format(self.start_date, self.end_date)
@@ -54,73 +81,32 @@ class Extract:
         
         return api_url
 
-    def set_start_date(self, new_start: date) -> None:
-        """
-        Setter method for start date
-        """
-        if isinstance(new_start, date):
-            new_start_str = new_start.strftime(self._DATE_FMT)
-            self.start_date = new_start_str
-        else:
-            raise TypeError(f"Expected new start date as date, got {type(new_start)} instead.")
-        
-        # update api url
-        self.api_url = self._create_api_url()
-
-    def set_end_date(self, new_end: date) -> None:
-        """
-        Setter method for end date
-        """
-        if isinstance(new_end, date):
-            new_end_str = new_end.strftime(self._DATE_FMT)
-            self.end_date = new_end_str
-        else:
-            raise TypeError(f"Expected new end date as date, got {type(new_end)} instead.")
-
-        # update api url
-        self.api_url = self._create_api_url()
-
-
-    def fetch_data(self) -> Dict[str, Dict[str, Any]]:
-        """
-        Fetch fx data based on start and end dates provided in the constructor
-        :return: JSON data retrieved from the API
-        """
+    def fetch_exchange_rate_data(self) -> None:
         response = requests.get(self.api_url)
-        if response.status_code != 200:
-            print(f">>> ERROR: request returned status code: {response.status_code}")
-        
-        return response.status_code, response.json()
 
-    def save_raw_data(self, data: dict, failed=False):
+        self.exchange_rates_dict = response.json()
+        self.api_call_status_code = response.status_code
+
+    def save_raw_data(self) -> None:
         now = datetime.now().strftime(self._DATETIME_FMT)
+        failed: bool = (self.api_call_status_code != 200)
+
         if failed:
             root = "/extracted-failed/"
+            # here logging of error
         else:
             root = "/extracted/"
 
-        data = json.dumps(data).encode("utf-8")
+        exchange_rates_dict = json.dumps(self.exchange_rates_dict).encode("utf-8")
         save_name = root + f"FXCADUSD_{now}.json"
-        self.dbx.files_upload(data, save_name, mode=dropbox.files.WriteMode.overwrite)
+
+        self.dbx.files_upload(exchange_rates_dict, save_name, mode=dropbox.files.WriteMode.overwrite)
 
     def download_expenses(self) -> None:
-        # TODO logging of metadata
-        self.dbx.files_download_to_file("data/expenses.xlsx", "/expenses.xlsx")
+        self.dbx.files_download_to_file(self._PATH_TO_EXPENSES, "/expenses.xlsx")
 
-    def extract_expenses(
-                         self, 
-                         path_to_expenses: Union[bytes, str, os.PathLike] = "data/expenses.xlsx"
-                         ) -> petl.Table:
-        """
-        Extracting expenses table from expenses excel file.
-
-        :param path_to_expenses: path identifier to the expenses table source file
-        :returns: petl-style data table with expenses data
-        :raies FileNotFoundError: if designated file does not exist 
-        """
-
-        if not isinstance(path_to_expenses, Path):
-            path_to_expenses = Path(path_to_expenses)
+    def extract_expenses(self) -> None:
+        path_to_expenses = Path(self._PATH_TO_EXPENSES)
 
         if not path_to_expenses.exists():
             raise FileNotFoundError(f"File {path_to_expenses} does not exist.")
@@ -131,66 +117,111 @@ class Extract:
             ".csv": petl.io.fromcsv
         }[path_to_expenses.suffix]
 
-        return loading_mode(str(path_to_expenses))
+        self.expenses_petl_table = loading_mode(str(path_to_expenses))
+
+    def run(self) -> None:
+        self.fetch_exchange_rate_data()
+
+        self.save_raw_data()
+
+        if self.api_call_status_code != 200:
+            sys.exit()
+
+        self.download_expenses()
+
+        self.extract_expenses()
+
+    def result(self) -> Tuple[dict, petl.Table]:
+        return self.exchange_rates_dict, self.expenses_petl_table
 
 
-class Transform:
-    """
-    Objects of this type deal with data extracted from BOC servers.
-    They will receive raw JSON-styled objects as input.
-    As an output they will be able to produce table-like
-    dataf structures ready to be loaded into a data wharehouse.
+class Transform(PipelineComponent):
 
-    This class encapsulates the TRANSFORM part of the ETL process.
-    """
+    def __init__(self, exchange_dict: dict, expenses_table: petl.Table) -> None:
+        self.exchange_dict = exchange_dict
+        self.expenses_petl_table = expenses_table
 
-    def __init__(self, raw_data: dict) -> None:
-        self._raw_data = raw_data
-        try:
-            self._data = raw_data["observations"]
-        except KeyError as e:
-            raise KeyError("Provided data lacks the appropriate key (observations)") from e
+        # intermediate variables
+        self.exchange_petl_table: petl.Table
 
-    def set_data(self, new_data: dict) -> None:
-        self._raw_data = new_data
-        self._data = new_data["observations"]
+        # out variable that will flow to loading step
+        self.out_table: petl.Table
 
-    def get_data(self) -> List:
-        return self._data
-
-    def _parse_data(self) -> Tuple[List]:
+    def create_exchange_petl_table(self) -> None:
         boc_dates = []
         boc_rates = []
-
-        for item in self._data:
+        for item in self.exchange_dict["observations"]:
             boc_dates.append(
                 datetime.strptime(item["d"], "%Y-%m-%d")
             )
 
             boc_rates.append(
-                item["FXUSDCAD"]["v"]
+                decimal.Decimal(item["FXUSDCAD"]["v"])
             )
 
-        return boc_dates, boc_rates
+        self.exchange_petl_table = petl.fromcolumns([boc_dates, boc_rates], header=['date','rate'])
 
-    def create_table(self) -> petl.Table:
-        dates, rates = self._parse_data()
-        table = petl.fromcolumns([dates, rates], header=['date','rate'])
+    def join_tables(self) -> None:
+        self.out_table = petl.outerjoin(
+            self.expenses_petl_table, 
+            self.exchange_petl_table,
+            key='date'
+            )
 
-        return table
+        self.out_table = petl.filldown(self.out_table, "rate")
+        
+        self.out_table = petl.select(
+            self.out_table,
+            lambda rec: rec.USD is not None
+            )
+    
+    def add_cad_field(self) -> None:
+        self.out_table = petl.addfield(
+            self.out_table, 
+            "CAD", 
+            lambda rec: decimal.Decimal(rec.USD) * rec.rate if rec.rate is not None else 0 
+            )
 
-    def join_tables(self, expenses_table: petl.Table, rate_table: petl.Table) -> petl.Table:
-        joined_table = petl.outerjoin(expenses_table, rate_table, key='date')
-        petl.filldown(joined_table, "rate")
+    def run(self) -> None:
+        self.create_exchange_petl_table()
+        self.join_tables()
+        self.add_cad_field()
 
-        return joined_table
+    def result(self) -> Tuple[petl.Table]:
+        return self.out_table,
 
 
-class Load:
+class Load(PipelineComponent):
+    _TABLE_NAME = "expenses_v2"
+    _DB_FILE = r"db\expenses.db"
 
-    def __init__(self, transformed_table: petl.Table) -> None:
+    def __init__(self, transformed_table: petl.Table, db_file:str, table_name:str) -> None:
         self.table = transformed_table
+        self.db_file = db_file
+        self.table_name = table_name
 
-    def load_to_db(self, conn, table_name: str = "expenses"):
-        petl.todb(self.table, conn, table_name)
-        conn.close()
+        self.db_conn: sqlite3.Connection
+
+        breakpoint()
+
+    def create_db_connection(self) -> None:
+        """ create a database connection to a SQLite database """
+        
+        try:
+            self.db_conn = sqlite3.connect(self.db_file)
+            print("SQLTE3 connection open")
+        except sqlite3.Error as e:
+            print(e)
+
+    def load_to_db(self) -> None:
+        petl.todb(self.table, self.db_conn, self.table_name)
+
+    def run(self) -> None:
+        try:
+            self.create_db_connection()
+            self.load_to_db()
+        finally:
+            self.db_conn.close()
+
+    def result(self) -> None:
+        print("Data successfully loaded")
