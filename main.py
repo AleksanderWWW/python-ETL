8ae7133bfb36592895f550f475932c5ac1e7d547
AleksanderWWW <alwojnarowicz@gmail.com>
2022-07-21 10:33:17 +0200

    reflect changes in etl in main

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ Main runnable module
 import yaml
 import dropbox
 
-from etl.etl_pipeline import run_pipeline
-from db_utils import create_connection
+from etl.src import Extract, Transform, Load
+from etl.etl_pipeline import Pipeline
 
 # read config
 with open("config.yaml", "r", encoding="utf-8") as config_fp:
@@ -16,16 +16,27 @@ with open("api-token.txt", "r") as fp:
     _API_TOKEN = fp.read()
     _DBX = dropbox.Dropbox(_API_TOKEN)
 
-_DB_CONN = create_connection(_CONFIG["DB"]["path_to_db"])
 
 
 def main():
-    try:
-        run_pipeline(_CONFIG, _DBX, _DB_CONN)
-    finally:
-        # close resources
-        _DB_CONN.close()
+    inputs = [
+        (Extract, None), 
+        (Transform, None), 
+        (Load, ("db\expenses.db", "expenses"))
+        ]
+    init_args = (
+        _CONFIG["BOC"]["url"],
+        _DBX,
+        _CONFIG["BOC"]["startDate"],
+        _CONFIG["BOC"]["endDate"]
+        )
 
+    etl_pipeline = Pipeline(
+        inputs,
+        *init_args
+    )
+
+    etl_pipeline.run_pipeline()
 
 
 if __name__ == "__main__":
